0de2c02ee9ea415bfcaceadf8926b7a3d31d807a
Andrey Shakel <shakel2000@yandex.ru>
2021-04-19 22:18:02 +0300

    Added experiments with two attention models based on LSTM and GRU

--- Utils/utils.py
+++ Utils/utils.py
@@ -1,4 +1,6 @@
+import gc
 import numpy as np
+import torch
 import torch.nn as nn
 from IPython.display import clear_output
 from matplotlib import pyplot as plt
@@ -6,26 +8,32 @@ from sklearn.metrics import accuracy_score, precision_score, recall_score, roc_c
 from tqdm import tqdm
 
 
-def compute_loss(batch_pred, batch_true):
-    return nn.CrossEntropyLoss()(batch_pred, batch_true - 1)
+def compute_loss(batch_pred, batch_true, device=torch.device('cuda:0')):
+    return nn.CrossEntropyLoss()(batch_pred, batch_true.to(device))
+
+
+def clip_batch(batch, size=500):
+    texts = batch[1][:, :size]
+    texts[:, -1] = 2
+    return (batch[0], texts)
 
 
 def get_all_y(model, data, predict_proba=False):
     y_true = []
     y_pred = []
     for batch in data:
-        y_true.extend(list((batch.label - 1).cpu().detach().numpy()))
+        y_true.extend(list((batch[0]).cpu().detach().numpy()))
         if not predict_proba:
-            y_pred.extend(list(model.predict(batch.text).cpu().detach().numpy()))
+            y_pred.extend(list(model.predict(batch[1]).cpu().detach().numpy()))
         else:
-            y_pred.extend(list(model(batch.text).cpu().detach().numpy()))
+            y_pred.extend(list(model(batch[1]).cpu().detach().numpy()))
     return np.array(y_true), np.array(y_pred)
 
 
 def get_scores(model, data, print_scores=False, from_iter=False):
     if not from_iter:
-        y_true = (data.label - 1).cpu().detach().numpy()
-        y_pred = model.predict(data.text).cpu().detach().numpy()
+        y_true = (data[0]).cpu().detach().numpy()
+        y_pred = model.predict(data[1]).cpu().detach().numpy()
     else:
         y_true, y_pred = get_all_y(model, data)
 
@@ -58,21 +66,24 @@ def plot_roc_curve(model, data):
     plt.show()
 
 
-def train_model(model, opt, train_iter, val_iter, n_epochs=1):
+def train_model(model, opt, train_iter, val_iter, device=torch.device('cuda:0'), n_epochs=1):
     metrics = {'train_loss': [], 'val_loss': [], 'val_accuracy': [], 'grad_norm': []}
 
     step = 0
     for i in tqdm(range(n_epochs)):
         for batch in train_iter:
+            batch = clip_batch(batch)
             step += 1
             model.train()
 
-            loss_t = compute_loss(model(batch.text), batch.label)
+            loss_t = compute_loss(model(batch[1]), batch[0], device=device)
             loss_t.backward()
             grad_norm = nn.utils.clip_grad_norm_(model.parameters(), 10)
 
             opt.step()
             opt.zero_grad()
+
+            gc.collect()
 
             if step % 100 == 99:
                 metrics['grad_norm'].append(grad_norm)
@@ -81,19 +92,20 @@ def train_model(model, opt, train_iter, val_iter, n_epochs=1):
 
                 val_losses = []
                 for val_batch in val_iter:
-                    val_losses.append(compute_loss(model(val_batch.text), val_batch.label).item())
+                    val_batch = clip_batch(val_batch)
+                    val_losses.append(compute_loss(model(val_batch[1]), val_batch[0], device=device).item())
                 metrics['val_loss'].append(np.mean(np.array(val_losses)))
 
                 val_true, val_pred = get_all_y(model, val_iter)
                 metrics['val_accuracy'].append(accuracy_score(val_true, val_pred))
 
-        model.eval()
-        clear_output(True)
-        plt.figure(figsize=(16, 4))
-        for i, (name, history) in enumerate(sorted(metrics.items())):
-            plt.subplot(1, len(metrics), i + 1)
-            plt.title(name)
-            plt.plot((np.arange(len(history)) + 1) * 100, history)
-            plt.grid()
-        plt.show()
-        print("Mean loss=%.3f" % np.mean(metrics['train_loss'][-5:], axis=0), flush=True)
+                model.eval()
+                clear_output(True)
+                plt.figure(figsize=(16, 4))
+                for i, (name, history) in enumerate(sorted(metrics.items())):
+                    plt.subplot(1, len(metrics), i + 1)
+                    plt.title(name)
+                    plt.plot((np.arange(len(history)) + 1) * 100, history)
+                    plt.grid()
+                plt.show()
+                print("Mean loss=%.3f" % np.mean(metrics['train_loss'][-5:], axis=0), flush=True)
